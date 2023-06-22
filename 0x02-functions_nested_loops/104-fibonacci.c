297c2ead95d0134eab6a1901017d31689fd23ea4
AYOUB <ayoubca06@gmail.com>
2023-06-22 03:08:55 -0700

    I sometimes suffer from insomnia

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -11,7 +11,7 @@ int main(void)
 {
 	int count;
 	unsigned long fib1 = 0, fib2 = 1, sum;
-	unsigned long fib1_half1, fib1_half2, fib2_half2;
+	unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
 	unsigned long half1, half2;
 
 	for (count = 0; count < 92; count++)
@@ -22,12 +22,12 @@ int main(void)
 	fib2 = sum;
 	}
 	fib1_half1 = fib1 / 10000000000;
-	fib2_half2 = fib2 / 10000000000;
+	fib2_half1 = fib2 / 10000000000;
 	fib1_half2 = fib1 % 10000000000;
 	fib2_half2 = fib2 % 10000000000;
 	for (count = 93; count < 99; count++)
 	{
-	half1 = fib1_half1 + fib2_half2;
+	half1 = fib1_half1 + fib2_half1;
 	half2 = fib1_half2 + fib2_half2;
 	if (fib1_half2 + fib2_half2 > 9999999999)
 	{
@@ -37,9 +37,9 @@ int main(void)
 	printf("%lu%lu", half1, half2);
 	if (count != 98)
 	printf(", ");
-	fib1_half1 = fib2_half2;
+	fib1_half1 = fib2_half1;
 	fib1_half2 = fib2_half2;
-	fib2_half2 = half1;
+	fib2_half1 = half1;
 	fib2_half2 = half2;
 	}
 	printf("\n");
