1e7cac00b0c7888b8fe26e4b16e87e4aa77e7b27
AYOUB <ayoubca06@gmail.com>
2023-06-21 18:59:50 -0700

    104-fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -22,12 +22,12 @@ int main(void)
 	fib2 = sum;
 	}
 	fib1_half1 = fib1 / 10000000000;
-	fib2_half2 = fib2 / 10000000000;
+	fib2_half1 = fib2 / 10000000000;
 	fib1_half2 = fib1 % 10000000000;
 	fib2_half2 = fib2 % 10000000000;
 	for (count = 93; count < 99; count++)
 	{
-	half1 = fib1_half1 + fib2_half1;
+	half1 = fib1_half1 + fib2_half2;
 	half2 = fib1_half2 + fib2_half2;
 	if (fib1_half2 + fib2_half2 > 9999999999)
 	{
