705fb037f25a29cd086947994a525ef1ea3e6e2a
AYOUB <ayoubca06@gmail.com>
2023-06-23 19:10:05 -0700

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,35 +1,39 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * main - print numbers 1 - 100 followed by a new line
- * numbers that are multiples of 3 print Fizz
- * numbers that are multiples of 5 print Buzz
- * numbers are that multiples of 3 and 5 print FizzBuzz
- * each number and word to be separeted by space
- *
+ * main - prints a Fizz Buzz program
  * Return: Always 0 (Success)
  */
 
 int main(void)
+
 {
-	int num;
+	int x;
 
-	for (num = 1; num <= 100; num++)
+	for (x = 1; num <= 100; num++)
 	{
-		if (num % 3 == 0 && !(num % 5 == 0))
-			printf("Fizz");
-		else if (num % 5 == 0 && !(num % 3 == 0))
-			printf("Buzz");
-		else if (num % 3 == 0 && num % 5 == 0)
-			printf("FizzBuzz");
-		else
-			printf("%d", num);
-
-		if (num != 100)
-			prontf(" ");
-		else
-			printf("\n");
+	if ((x % 3 == 0) && (x % 5 == 0))
+	{
+	printf("FizzBuzz");
 	}
-
-	return (0)
+	else if (x % 3 == 0)
+	{
+	printf("Fizz");
+	}
+	else if (x % 5 == 0)
+	{
+	printf("Buzz");
+	}
+	else
+	{
+	printf("%d", x);
+	}
+	if (x != 100)
+	{
+	printf(" ");
+	}
+	}
+	printf("\n");
+	return (0);
 }
